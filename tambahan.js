6453a1b986936d292f7938f1ed7931e9fda6f217
ilyaswilian <74714631+ilyaswilian@users.noreply.github.com>
2020-12-20 08:20:39 +0700

    Update tambahan.js

--- tambahan.js
+++ tambahan.js
@@ -1,9 +1,10 @@
 module.exports = {
 	halo: "HALO JUGA ZEYENKK* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	hai: "HAI JUGAA!*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	ass: "WAALAIKUMSALAM*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	ass: "salam gaboleh disingkat zheyeng*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	bro: "EUUUYYYY!!!* \n\nketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
-	p: "pa pe pa pe pap pap*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
+	p: "pa pe pa pe salam yang bener anjg*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	test: "*satu dua dicoba*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 	sbr: "SABAR BOII LAGI DICARIIN",
+        assalamualaikum: "Waalaikumsalam Wr.wb*\n\n ketik *!menu* untuk menampilkan semua fitur yang ada \r\n\r\n \n \r",
 }
